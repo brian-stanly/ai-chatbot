ba7eb8ef3d0b820df3c8fc597455353a15e1fc22
brianstanly <brianstanly@outlook.com>
2026-05-19 22:56:13 +0530

    create serializers

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,10 +1,14 @@
+import datetime
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class Message(BaseModel):
     role: str  # "user" or "assistant"
     content: str
+
+    class Config:
+        from_attributes = True
 
 
 class ChatRequest(BaseModel):
@@ -12,4 +16,23 @@ class ChatRequest(BaseModel):
 
 
 class ChatResponse(BaseModel):
-    companyName: str
+    reply: str
+
+
+class ChatInput(BaseModel):
+    content: str
+
+class ConverstionCreate(BaseModel):
+    title: Optional[str] = "New Converstion"
+
+class ConversationTitleUpdate(BaseModel):
+    title: str
+
+
+class ConverstationResponse(BaseModel):
+    id: str
+    title: str
+    created_at: datetime.datetime
+
+    class Config:
+        from_attributes = True
