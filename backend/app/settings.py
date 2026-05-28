62b54e977002052ba6563a6e3e648aa748fe27b5
brianstanly <brianstanly@outlook.com>
2026-05-28 19:52:27 +0530

    migrate to postgres

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -66,9 +66,12 @@ ASGI_APPLICATION = 'app.asgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'chatbot.db',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
