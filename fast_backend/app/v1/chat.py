ce154eb8921cd78a4d6023dd40cf4b8722341855
brianstanly <brianstanly@outlook.com>
2026-05-30 20:31:06 +0530

    created enpoint for chat session

--- fast_backend/app/v1/chat.py
+++ fast_backend/app/v1/chat.py
@@ -3,14 +3,39 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from db.database import get_db
-from models.message import Message as ChatMessage
-from schemas.chat_schema import Message
+from models.message import ChatMessage, ChatSession
+from schemas.chat_schema import (
+    ChatMessageResponse, 
+    ChatSessionResponse,
+    ChatSessionRequest
+)
+
 router = APIRouter()
 
 @router.get("/")
 def home():
     return {"message": "Welcome to the Production FastAPI App"}
 
-@router.get("/session")
-def get_message(db: Session = Depends(get_db)) -> List[Message]:
+# retive all the messages
+@router.get("v1/session/list")
+def get_message(db: Session = Depends(get_db)) -> List[ChatMessageResponse]:
     return db.query(ChatMessage).all()
+
+# api for creating sesssion
+@router.post("v1/session/create")
+def create_session(
+    title: ChatSessionRequest,
+    db: Session = Depends(get_db)) -> ChatSessionResponse:
+
+    record = ChatSession(
+        title = title.title
+    )
+    db.add(record)
+    db.commit()
+    db.refresh(record)
+    return record
+
+
+# api for llm response
+
+
